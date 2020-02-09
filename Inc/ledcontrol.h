1b84b3e991f32a21697a85bf6a3bd53275b5f3aa
DDHunter13 <1310nero@mail.ru>
2020-02-10 00:17:52 +0300

    Add comments to ledcontrol.c/.h

--- Inc/ledcontrol.h
+++ Inc/ledcontrol.h
@@ -12,9 +12,16 @@
 #include <cmsis_os.h>
 #include <main.h>
 
-uint8_t led_state;
+uint8_t led_state; //!< Current Led state. Need to switch by function.
 
+/** @brief The function to provide blink led.
+ *
+ */
 void Led_Blink();
+
+/** @brief The function to switch led state.
+ * @note It changes led_state variable.
+ */
 void Led_Switch();
 
 #endif /* LEDCONTROL_H_ */
--- Src/ledcontrol.c
+++ Src/ledcontrol.c
@@ -9,18 +9,25 @@
 
 void Led_Blink()
 {
+	// remember old state to returns to it after blink
 	uint8_t old_state = led_state;
+	// off
 	HAL_GPIO_WritePin(STATUS_INDICATE_PORT, STATUS_INDICATE_PIN, GPIO_PIN_SET);
 	osDelay(LED_BLINK_DELAY);
+	// on
 	HAL_GPIO_WritePin(STATUS_INDICATE_PORT, STATUS_INDICATE_PIN, GPIO_PIN_RESET);
 	osDelay(LED_BLINK_DELAY);
+	// off
 	HAL_GPIO_WritePin(STATUS_INDICATE_PORT, STATUS_INDICATE_PIN, GPIO_PIN_SET);
 	osDelay(LED_BLINK_DELAY);
+	// return old
 	HAL_GPIO_WritePin(STATUS_INDICATE_PORT, STATUS_INDICATE_PIN, old_state);
 }
 
 void Led_Switch()
 {
+	// Switch led_state variable
 	led_state = (1 == led_state)? 0 : 1;
+	// Switch HW led state
 	HAL_GPIO_WritePin(STATUS_INDICATE_PORT, STATUS_INDICATE_PIN, led_state);
 }
