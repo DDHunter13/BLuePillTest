2c6f99afd38562dddf29dc94f06485dd622e21a4
DDHunter13 <1310nero@mail.ru>
2020-02-10 00:21:40 +0300

    Add comments to adc_control.c/.h

--- Inc/adc_control.h
+++ Inc/adc_control.h
@@ -13,8 +13,14 @@
 extern ADC_HandleTypeDef hadc1;
 extern ADC_HandleTypeDef hadc2;
 
-
+/** @brief The function to get measure of channel0 of adc2 and calculate the voltage.
+ * @retval Measured voltage.
+ */
 float adc2_measure();
+
+/** @brief The function to get measure of temperature sensor of adcq and calculate the temperature.
+ * @retval Measured temperature.
+ */
 float adc1_temp_measure();
 
 #endif /* ADC_CONTROL_H_ */
--- Src/adc_control.c
+++ Src/adc_control.c
@@ -9,20 +9,30 @@
 
 float adc2_measure()
 {
+	// Start the adc measure
 	HAL_ADC_Start(&hadc2);
+	// Waiting for conversion
 	HAL_ADC_PollForConversion(&hadc2,100);
+	// Get measured value
 	float adc_res = (float)HAL_ADC_GetValue(&hadc2);
+	// Stop adc
 	HAL_ADC_Stop(&hadc2);
+	// Calculate the real voltage
 	adc_res = adc_res / 4096 * 3.3;
 	return adc_res;
 }
 
 float adc1_temp_measure()
 {
+	// Start the adc measure
 	HAL_ADC_Start(&hadc1);
+	// Waiting for conversion
 	HAL_ADC_PollForConversion(&hadc1,100);
+	// Get measured value
 	float adc_res = (float)HAL_ADC_GetValue(&hadc1);
+	// Stop adc
 	HAL_ADC_Stop(&hadc1);
+	// Calculate the real temperature
 	adc_res = adc_res / 4096 * 3.3;
 	float sub = adc_res - 1.43;
 	sub = sub / 0.0043;
